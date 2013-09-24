26c7179a0064a9f057869c4227fff4ff61b30c68
Persa Zula <persa@persazula.com>
2013-09-24 09:28:03 -0600

    Satisfies Customer initialized from a hash of data test

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -3,9 +3,14 @@ require 'minitest/autorun'
 require 'minitest/pride'
 
 class Customer
+  attr_reader :id, :first_name, :last_name, :created_at, :updated_at
 
   def initialize(input = {})
-
+    @id = input[:id]
+    @first_name = input[:first_name]
+    @last_name = input[:last_name]
+    @created_at = input[:created_at]
+    @updated_at = input[:updated_at]
   end
 
 end
